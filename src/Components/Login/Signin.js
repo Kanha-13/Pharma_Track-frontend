0589926be570e8217fde2e846e4234bbc0d78f06
Kanha Agrawal <kanha.agr11@gmail.com>
2023-06-19 14:12:54 +0530

    added loading screens on signup and signin

--- src/Components/Login/Signin.js
+++ src/Components/Login/Signin.js
@@ -4,8 +4,10 @@ import './sigin.css'
 import Ganpatiji from "../../images/ganpatiji.svg"
 import { handleLoginSubmit } from '../../apis/login';
 import { ROUTES } from '../../Constants/routes_frontend';
+import Loading from '../Loading/Loading';
 
 const Signin = ({ onSwitch }) => {
+  const [loading, setLoading] = useState(false);
   const [isDisabled, setisDisabled] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -13,6 +15,7 @@ const Signin = ({ onSwitch }) => {
 
   const onloginclick = async (e) => {
     e.preventDefault()
+    setLoading(true)
     setisDisabled(true)
     const data = {
       email: email,
@@ -32,26 +35,28 @@ const Signin = ({ onSwitch }) => {
         alert(error.response?.data.Error_message)
       else
         alert("Something went wrong!")
+      setLoading(false)
       setisDisabled(false)
     }
   }
   return (
-    <div className="login-child-container">
-      <form onSubmit={onloginclick} className='left'>
-        <h2 style={{ fontSize: "2.1rem" }}>Welcome back!</h2>
-        <p style={{ margin: "0.7vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>E-mail address</p>
-        <input required placeholder='example@example.com' autoFocus type='email' style={{ padding: "0px 2%", fontSize: "1.1rem", margin: "5px 0px", height: "5%", width: "46%", border: "2px solid #D6D8E7", borderRadius: "0.2rem", fontSize: "1.2rem" }} value={email} onChange={(e) => setEmail(e.target.value)} />
-        <p style={{ margin: "1.4vh 0px 0.7vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>Password</p>
-        <input required placeholder="***********" type='password' style={{ padding: "0px 2%", fontSize: "1.1rem", margin: "5px 0px", height: "5%", width: "46%", border: "2px solid #D6D8E7", borderRadius: "0.2rem", fontSize: "1.2rem" }} value={password} onChange={(e) => setPassword(e.target.value)} />
-        <p onClick={()=>onSwitch(4)} style={{ margin: "1.4vh 0px 4.2vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem", color: "#489BE8", cursor: "pointer" }}>forget password?</p>
-        <button disabled={isDisabled} type='submit' style={{ cursor: "pointer", backgroundColor: "#5E48E8", color: "#ffffff", width: "50%", height: "5%", borderRadius: "0.2rem", outline: "none", border: "none" }}>Continue</button>
-        <p style={{ margin: "4.2vh 0px 4.2vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>New here?<span onClick={() => onSwitch(0)} style={{ marginLeft: "5px", cursor: "pointer", color: "#489BE8" }}>Register</span></p>
-      </form>
-      <div className='right'>
-        <img src={Ganpatiji} />
-        <p>Agrawal Medical</p>
+    loading ? <Loading /> :
+      <div className="login-child-container">
+        <form onSubmit={onloginclick} className='left'>
+          <h2 style={{ fontSize: "2.1rem" }}>Welcome back!</h2>
+          <p style={{ margin: "0.7vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>E-mail address</p>
+          <input required placeholder='example@example.com' autoFocus type='email' style={{ padding: "0px 2%", fontSize: "1.1rem", margin: "5px 0px", height: "5%", width: "46%", border: "2px solid #D6D8E7", borderRadius: "0.2rem", fontSize: "1.2rem" }} value={email} onChange={(e) => setEmail(e.target.value)} />
+          <p style={{ margin: "1.4vh 0px 0.7vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>Password</p>
+          <input required placeholder="***********" type='password' style={{ padding: "0px 2%", fontSize: "1.1rem", margin: "5px 0px", height: "5%", width: "46%", border: "2px solid #D6D8E7", borderRadius: "0.2rem", fontSize: "1.2rem" }} value={password} onChange={(e) => setPassword(e.target.value)} />
+          <p onClick={() => onSwitch(4)} style={{ margin: "1.4vh 0px 4.2vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem", color: "#489BE8", cursor: "pointer" }}>forget password?</p>
+          <button disabled={isDisabled} type='submit' style={{ cursor: "pointer", backgroundColor: "#5E48E8", color: "#ffffff", width: "50%", height: "5%", borderRadius: "0.2rem", outline: "none", border: "none" }}>Continue</button>
+          <p style={{ margin: "4.2vh 0px 4.2vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>New here?<span onClick={() => onSwitch(0)} style={{ marginLeft: "5px", cursor: "pointer", color: "#489BE8" }}>Register</span></p>
+        </form>
+        <div className='right'>
+          <img src={Ganpatiji} />
+          <p>Agrawal Medical</p>
+        </div>
       </div>
-    </div>
   );
 }
 export default Signin;
