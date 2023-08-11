21254a28fc9d929ff6869916f14da78a02c421af
kanha <kanha.agr11@gmail.com>
2023-08-11 20:52:17 +0530

    added more products category

--- src/Constants/productCategories.js
+++ src/Constants/productCategories.js
@@ -1,8 +1,20 @@
 export const ProductCategories = [
   { value: "TABLET", label: "TABLET" },
-  { value: "BOTTLE", label: "BOTTLE" },
+  { value: "SYRUP", label: "SYRUP" },
+  { value: "CREME", label: "CREME" },
   { value: "TUBE", label: "TUBE" },
-  { value: "INJ_VIAL", label: "INJECTION VIAL" },
+  { value: "INJECTION", label: "INJECTION" },
+  { value: "GEL", label: "GEL" },
+  { value: "DROPS", label: "DROPS" },
+  { value: "OIL", label: "OIL" },
+  { value: "JAR", label: "JAR" },
+  { value: "CHURAN", label: "CHURAN" },
+  { value: "SACHET", label: "SACHET" },
+  { value: "SUSPENSION", label: "SUSPENSION" },
+  { value: "SOLUTION", label: "SOLUTION" },
+  { value: "OINTMENT", label: "OINTMENT" },
+  { value: "GRANULES", label: "GRANULES" },
+  { value: "LIQUID", label: "LIQUID" },
   { value: "UNIT", label: "UNIT" },
 ]
 
