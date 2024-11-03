67bb006fed893ed4298d2b458797eb7e33645a70
kanha-13 <kanha.agr11@gmail.com>
2024-11-03 23:26:15 +0530

    added dummy otp to work for signup without otp verification

--- src/Components/Login/Otp.js
+++ src/Components/Login/Otp.js
@@ -45,7 +45,7 @@ const OTP = ({ onSwitch, data = {} }) => {
     <div className="login-child-container">
       <form onSubmit={onOTPsubmit} className="left">
         <h2 style={{ fontSize: "2.1rem" }}>Enter OTP</h2>
-        <p style={{ margin: "5px 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>Otp</p>
+        <p style={{ margin: "5px 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>OTP</p>
         <input required placeholder='ex:123456' autoFocus type='text' style={{ padding: "0px 2%", fontSize: "1.1rem", margin: "5px 0px", height: "5%", width: "46%", border: "2px solid #D6D8E7", borderRadius: "0.2rem", fontSize: "1.2rem" }} value={otp} onChange={(e) => setOTP(e.target.value)} />
         <button disabled={isDisabled} type="submit" style={{ marginTop: "30px", cursor: "pointer", backgroundColor: "#5E48E8", color: "#ffffff", width: "50%", height: "5%", borderRadius: "0.2rem", outline: "none", border: "none" }}>Continue</button>
         <p style={{ margin: "4.2vh 0px 0.5vh 0px", width: "50%", textAlign: "left", fontSize: "0.9rem" }}>Want to change email?<span onClick={changeEmail} style={{ marginLeft: "5px", cursor: "pointer", color: "#489BE8" }}> click here</span></p>
